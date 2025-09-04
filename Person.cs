ec2df326fd9ba9096d79a8e1ed8d36d924abf210
AxZoRos <zenjakonovalov154@gmail.com>
2025-09-05 00:39:19 +0300

    add project
    Додано проєкт

---
+++ Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB16_KTP
+{
+    internal class Person
+    {
+         // Приватні поля
+        private string name;
+        private int age;
+        // Властивість для доступу до name
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        // Властивість для age з валідацією
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Вік не може бути від'ємним.");
+                age = value;
+            }
+        }
+        // Конструктор класу Person
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        // Метод для порівняння віку з іншою особою
+        public int CompareAge(Person other)
+        {
+            return this.Age - other.Age;
+        }
+        // Метод для виведення базової інформації про особу
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Ім'я: {Name}, Вік: {Age}");
+        }
+    }
+    // Клас-нащадок Child з додатковою властивістю для школи
+    class Child : Person
+    {
+        public string SchoolName { get; set; }
+
+        public Child(string name, int age, string schoolName)
+            : base(name, age)
+        {
+            SchoolName = schoolName;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Школа: {SchoolName}");
+        }
+    }
+    // Клас-нащадок Adult з додатковою властивістю для посади/роботи
+    class Adult : Person
+    {
+        public string Occupation { get; set; }
+
+        public Adult(string name, int age, string occupation)
+            : base(name, age)
+        {
+            Occupation = occupation;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Робота: {Occupation}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB16_KTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; 
+            // Створення базових об'єктів класу Person
+            Person person1 = new Person("Іван Петров", 30);
+            Person person2 = new Person("Олексій Коваленко", 25);
+
+            // Створення об'єктів похідних класів
+            Child child = new Child("Марія", 10, "Школа N1");
+            Adult adult = new Adult("Коновалов Євгеній", 35, "Програміст");
+
+            // Виведення інформації про двох осіб
+            Console.WriteLine("Порівняння віку між людьми:");
+            person1.DisplayInfo();
+            person2.DisplayInfo();
+
+            int ageDifference = person1.CompareAge(person2);
+            if (ageDifference > 0)
+                Console.WriteLine($"{person1.Name} старший за {person2.Name} на {ageDifference} років.");
+            else if (ageDifference < 0)
+                Console.WriteLine($"{person2.Name} старший за {person1.Name} на {-ageDifference} років.");
+            else
+                Console.WriteLine($"{person1.Name} та {person2.Name} мають однаковий вік.");
+
+            // Вивід розширеної інформації для дитини та дорослої особи
+            Console.WriteLine("\nІнформація про дитину:");
+            child.DisplayInfo();
+
+            Console.WriteLine("\nІнформація про дорослого:");
+            adult.DisplayInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
